1999311a7ef3a0e1a20466db8eba3dcbe6e5c665
Max Nichols <allenmaxnichols@gmail.com>
2022-02-07 16:47:54 -0600

    more changes

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,22 @@
+import { getAllEvents } from "../../dummy-data";
+import { useRouter } from "next/router";
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
 function AllEventsPage() {
+  const allEvents = getAllEvents();
+  const router = useRouter();
+
+  function searchHandler(year, month) {
+    router.push({
+      pathname: `/events/${year}/${month}`,
+    });
+  }
+
   return (
     <div>
       <h1>All Events</h1>
+      <EventsSearch onSearch={searchHandler} />
+      <EventList items={allEvents} />
     </div>
   );
 }
